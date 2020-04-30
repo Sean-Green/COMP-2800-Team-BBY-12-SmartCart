0db901485c31228cad5b9d6ef457a851c3fd30ca
jason <jasoncfleebcit@gmail.com>
2020-04-30 06:17:52 -0700

    list page fixed jquery bug where inputs left unfilled will break itemlist

--- Javascript/itemlist_page.js
+++ Javascript/itemlist_page.js
@@ -5,21 +5,35 @@ $(document).ready(function(){
     $('#theaddbutton').on('click', function() {
         let content =$('#listarea1').html();
         let itemname = $('#inputItem').val();
+        if( !$('#inputItem').val()){
+            itemname = "Empty";
+        }
         let p = "<li id=magicitem" + id + ">" + itemname + "</li>";
         $('#listarea1').html(content + p);
         $('#input1').appendTo('#magicitem' + id);
+        console.log("item is" + itemname);
 
         content =$('#listarea2').html();
         itemname = $('#inputWeight').val();
+        if( !$('#inputWeight').val()){
+            itemname = "Empty"
+        }
         p = "<li id=weightmagic" + id + ">" + itemname + "</li>";
         $('#listarea2').html(content + p);
         $('#input2').appendTo('#weightmagic' + id);
+        console.log("weight is is" + itemname);
+        
 
         content =$('#listarea3').html();
         itemname = $('#inputQuantity').val();
+        if( !$('#inputQuantity').val()){
+            itemname = "Empty"
+        }
         p = "<li id=quantitymagic" + id + ">" + itemname + "</li>";
         $('#listarea3').html(content + p);
         $('#input3').appendTo('#quantitymagic' + id);
+        console.log("quantity is" + itemname);
+        
         id++;
     })
 });
