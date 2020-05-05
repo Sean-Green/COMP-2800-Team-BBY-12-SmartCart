40647c3ef8cdbaa04ef2346883f9517fdd03934d
Sean <sean.joseph.green@gmail.com>
2020-05-05 14:04:30 -0700

    read updates

--- Javascript/proto.js
+++ Javascript/proto.js
@@ -1,23 +1,54 @@
-// Fucntion that creates a new document in the users collection
+// Function that creates a new document in the users collection
 function createUser() {
    // if the current user logged in user
    // is authenticated, then grab "uid" "displayName" and "email"
    // use "set()" with merge (if document did not exist it will be created)
    firebase.auth().onAuthStateChanged(function (user) {
-       db.collection("Users").doc(user.uid).set({
-           "name": user.displayName,
-           "email": user.email,
-       }, {
-           merge: true
-       });
+      db.collection("Users").doc(user.uid).set({
+         "name": user.displayName,
+         "email": user.email,
+      }, {
+         merge: true
+      });
    });
 }
 
+//How to get details about the current user, from the firebase API:
+//   We want 2 main things from this: 
+//             .displayName and 
+//             .uid
+function getUserDetails() {
+   firebase.auth().onAuthStateChanged(function (user) {
+      if (user) {
+         console.log(user.displayName);
+         console.log("Sign-in provider: " + user.providerId);
+         ///////////// IMPORTANT ////////////////////////////////
+         console.log("  Provider-specific UID: " + user.uid); 
+         console.log("  Name: " + user.displayName); 
+         ////////////////////////////////////////////////////////
+         console.log("  Email: " + user.email);
+         console.log("  Photo URL: " + user.photoURL);
+      } else {
+         console.log("user not signed in");
+      }
+   });
+};
+// Basic function reading user profile data and displaying it to a marked div.
+function getUserDisplayName() {
+   firebase.auth().onAuthStateChanged(function (user) {
+      if (user) {         
+      $('#HelloUser').text("Hello " + user.displayName);
+      } else {
+         console.log("user not signed in");
+         name = 'fran';
+      }
+   });
+};
 // logs the user out, should be called from a link to the splash page.
-function logOut(){
-    firebase.auth().signOut().then(function() {
-        console.log("Logout succeeds");
-      }, function(error) {
-        console.log("logout fails: " + error);
-      });
+function logOut() {
+   firebase.auth().signOut().then(function () {
+      console.log("Logout succeeds");
+   }, function (error) {
+      console.log("logout fails: " + error);
+   });
 }
