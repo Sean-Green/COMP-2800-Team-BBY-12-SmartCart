b4796e925557d6fd1cc538c28da0cd86e69650a3
kim <rlaehdrbs90@gmail.com>
2020-05-07 23:17:28 -0700

    read data and change

--- Javascript/shoping_page.js
+++ Javascript/shoping_page.js
@@ -1,12 +1,59 @@
-function lists() {
+let count = [];
+let unavilableCount = [];
+
+
+function countItem() {
     $(document).ready(function () {
         firebase.auth().onAuthStateChanged(function (user) {
+
             db.collection("Items").onSnapshot(function (doc) {
                 doc.forEach(function (item) {
-                    $('#products').append('<tr><td data-th="Product"><div class="row"><a href="https://placeholder.com"><img src="https://via.placeholder.com/100"></a><h4 class="nomargin">'+ item.get("name")+'</h4></div></td><td data-th=""></td><td data-th="Quantity"><input type="number" class="form-control text-center" value="1"></td><td data-th="Price" class="text-center">'+item.get("size")+'</td><td class="actions" data-th=""><button class="btn btn-info btn-sm"><i class="fa fa-refresh"></i></button><button class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></button></td></tr>');
+                    count.push(item.get("name"));
+                    console.log(count);
+                    console.log(count.length);
+                    document.getElementById('count').innerHTML = "Find Total available "
+                        + "<span style='color: blue;'>" + count.length + "</span>" + " items";
+                });
+            });
+            db.collection("unavailable").onSnapshot(function (doc) {
+                doc.forEach(function (item) {
+
+                    console.log(item.get("name"));
+
+                    unavilableCount.push(item.get("name"));
+                    console.log(unavilableCount);
+                    console.log(unavilableCount.length);
+                    document.getElementById('unavilableCount').innerHTML = "Find Total unavailable "
+                        + "<span style='color: red;'>" + unavilableCount.length + "</span>" + " items";
                 });
             });
         });
     })
 }
-lists();
+
+countItem();
+
+function toggleOnByInput() {
+     $('#myonoffswitch').prop('checked', true).change()
+     
+    $('#products').empty();
+
+    db.collection("Items").onSnapshot(function (doc) {
+        doc.forEach(function (item) {
+            $('#products').append('<tr><td data-th="Product"><div class="row"><a href="https://placeholder.com"><img src="https://via.placeholder.com/100"></a><h4 class="nomargin">' + item.get("name") + '</h4></div></td><td data-th=""></td><td data-th="Quantity"><input type="number" class="form-control text-center" value="1"></td><td data-th="Price" class="text-center">' + item.get("size") + '</td><td class="actions" data-th=""></td></tr>');
+        });
+    });
+}
+
+function toggleOffByInput() {
+    $('#myonoffswitch').prop('checked', false).change()
+
+    $('#products').empty();
+    db.collection("unavailable").onSnapshot(function (doc) {
+        doc.forEach(function (item) {
+            $('#products').append('<tr><td data-th="Product"><div class="row"><a href="https://placeholder.com"><img src="https://via.placeholder.com/100"></a><h4 class="nomargin">' + item.get("name") + '</h4></div></td><td data-th=""></td><td data-th="Quantity"><input type="number" class="form-control text-center" value="1"></td><td data-th="Price" class="text-center">' + item.get("size") + '</td><td class="actions" data-th=""></td></tr>');
+        });
+    });
+
+
+}
