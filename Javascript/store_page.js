8114f5448b85a85e75c9ec7ae9f82cd77cfdff73
Sean <sean.joseph.green@gmail.com>
2020-05-13 12:46:03 -0700

    working

--- Javascript/store_page.js
+++ Javascript/store_page.js
@@ -6,6 +6,7 @@ function storelists() {
             db.doc('Users/' + user.uid).get().then((userDoc) => {
                 db.collection("Stores").get().then(function (storeDocs) {
                     //for each store in the DB
+                    var idNum = 0;
                     storeDocs.forEach(function (store) {
                         var shoppingList;
                         var unavail;
@@ -16,7 +17,7 @@ function storelists() {
                                 shoppingList = userItems.docs
                                 unavail = unavailItems.docs
                                 pageHTML = '<div class="col-md-4"><div class="card mb-4 shadow-sm"><svg class="bd-placeholder-img card-img-top" width="100%" height="225"xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false"role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef"dy=".3em">Backgroud image</text></svg><div class="card-body"><h4>' 
-                                + store.get("name") + '</h4><p class="card-text">' + store.get("address") + '</p><div class="d-flex justify-content-between align-stores-center"><div class="btn-group"><button type="button" class="btn btn-sm btn-outline-secondary" data-toggle="modal"data-target="#myModal">Store detail</button><div class="modal fade" id="myModal" role="dialog"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h3 class="modal-title">Item</h3><h4 class="storeItemCount" id="medAva1">1/2 items</h4></div><div class="modal-body"><ul class="list-group list-group-flush"><li class="list-group-item">name<div class="storeItemAvailiablity">item avaliablity</div></li>'
+                                + store.get("name") + '</h4><p class="card-text">' + store.get("address") + '</p><div class="d-flex justify-content-between align-stores-center"><div class="btn-group"><button type="button" class="btn btn-sm btn-outline-secondary" data-toggle="modal"data-target="#myModal'+ idNum++ +'">Store detail</button><div class="modal fade" id="myModal" role="dialog"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h3 class="modal-title">Item</h3><h4 class="storeItemCount" id="medAva1">1/2 items</h4></div><div class="modal-body"><ul class="list-group list-group-flush"><li class="list-group-item">name<div class="storeItemAvailiablity">item avaliablity</div></li>'
                                 for (i = 0; i < shoppingList.length; i++) {
                                     for (j = 0; j < unavail.length; j++) {
                                         var thisItemName = shoppingList[i].get("name");
