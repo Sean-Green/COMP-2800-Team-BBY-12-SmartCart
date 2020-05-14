d85a803d5170adacef46b10716818c8894245c2a
jason <jasoncfleebcit@gmail.com>
2020-05-14 14:02:09 -0700

    quick change for pull of maha stuff

--- Javascript/itemlist_page.js
+++ Javascript/itemlist_page.js
@@ -1,19 +1,65 @@
 $(document).ready(function () {
 
-    var buildsaveliststatus = false;
-    console.log(buildsaveliststatus);
-    $(document).on('click', ".listNameBtn", function () {
-        buildsaveliststatus = true;
-        console.log(buildsaveliststatus);
-    })
-    console.log("round two" + buildsaveliststatus);
+    //get the user details
+    firebase.auth().onAuthStateChanged(function (user) {
+        //use the users ID to access their file
+        db.doc("Users/" + user.uid).get().then(function (userDoc) {
+            // set the store we will access to the users current store
+            var currentStore = userDoc.get("currentStore");
+            // set user current shopping list
+            var shoppingList = userDoc.get("shoppingList");
+            // html variable which we will append
+            var listHTML = "";
+
+            console.log("Happy feet " + shoppingList);
+            // lets you know on the page what store you will be writing to
+            // $('#itemListContainer').prepend('<b>' + currentStore + '</b>');
+
+            //////////////////////////////////////////////////////////////////////
+            //          BUILDS A LIST FROM THE DATABASE INTO HTML               //
+            //////////////////////////////////////////////////////////////////////
+            // this reads the collection of user items from the database and stores it in userList
+
+            // try {
+            //     db.collection('Users/' + user.uid + '/' + shoppingList).get().then(userList => {
+            //         // we turn the documents into an array
+            //         let item = userList.docs;
+            //         console.log(item);
+            //         // loop through the array and add an <li> item for each item
+            //         for (i = 0; i < item.length; i++) {
+            //             listHTML += '<li><span id="itemName' + i + '">' + item[i].get("name") + '</span>';
+            //             let itemName = item[i].get("name");
+            //             console.log(itemName);
+            //             listHTML += '<button id="remove' + i + '">&#9989;</button><button id="add' + i + '">&#10060;</button></li>';
+            //             $('#itemList').append(listHTML);
+            //             listHTML = "";
+            //             $(document).on('click', "#remove" + i, () => {
+            //                 console.log("removing " + itemName + " from " + currentStore + " unavailable list");
+            //                 removeItemFromUnavailable(itemName);
+            //             });
+            //             $(document).on('click', "#add" + i, () => {
+
+            //                 console.log("adding " + itemName + "  to " + currentStore + " unavailable list");
+            //                 addItemToUnavailable(itemName);
+            //             });
+            //         }
+            //     });
+            // } catch {
+            //     console.log("no List selected");
+            // }
+
+            ////////////////////////////////////////////////////////////////////////
+            ////////////////////////////////////////////////////////////////////////
+            ////////////////////////////////////////////////////////////////////////
+        });
+    });
 
 
 
 
 
-    //class listNameBtn
 
+/////////////////////////////////////////////////////////////////////////////////////
             //using arrays to access database information for item's name, size and unit
             var namearray = [];
             var sizearray = [];
