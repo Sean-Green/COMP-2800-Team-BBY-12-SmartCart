083b27162313c95844c99818522a66620e80a9cc
Sean <sean.joseph.green@gmail.com>
2020-05-19 18:32:30 -0700

    Shopping page front end mostly done

--- Javascript/shoping_page.js
+++ Javascript/shoping_page.js
@@ -1,200 +1,163 @@
-var shoppingMode= true;
-let count = [];
-let unavilableCount = [];
-
-/*
-function countItem() {
-    $(document).ready(function () {
-        firebase.auth().onAuthStateChanged(function (user) {
-
-            db.collection("Items").onSnapshot(function (doc) {
-                doc.forEach(function (item) {
-                    count.push(item.get("name"));
-                    console.log(count);
-                    console.log(count.length);
-                    //  document.getElementById('count').innerHTML = "Find Total available "
-                    //     + "<span style='color: blue;'>" + count.length + "</span>" + " items";
-                });
-            });
-            db.collection("unavailable").onSnapshot(function (doc) {
-                doc.forEach(function (item) {
-
-                    console.log(item.get("name"));
-
-                    unavilableCount.push(item.get("name"));
-                    console.log(unavilableCount);
-                    console.log(unavilableCount.length);
-                    //document.getElementById('unavilableCount').innerHTML = "Find Total unavailable "
-                    //    + "<span style='color: red;'>" + unavilableCount.length + "</span>" + " items";
-                });
-            });
-        });
-    })
-}
-
-countItem();
-*/
-
-let array = [];
-
 $(document).ready(() => {
-
     function displayList() {
-        $(document).ready(function () {
+        firebase.auth().onAuthStateChanged(function (user) {
+            db.doc("Users/" + user.uid).get().then(function (userDoc) {
+                let currentStore = userDoc.get("currentStore");
+                let shoppingList = userDoc.get("shoppingList");
+                let listHTML = "";
+                $('#storename').prepend('<h3>' + currentStore + '</h3>');
 
-            firebase.auth().onAuthStateChanged(function (user) {
-                db.doc("Users/" + user.uid).get().then(function (userDoc) {
-                    let currentStore = userDoc.get("currentStore");
-                    let shoppingList = userDoc.get("shoppingList");
-                    let listHTML = "";
-                    $('#storename').prepend('<h3>' + 'Your Store : ' + currentStore + '</h3>');
+                db.collection('Users/' + user.uid + '/' + shoppingList).get().then(userList => {
+                    let item = userList.docs;
+                    for (i = 0; i < item.length; i++) {
 
-                    db.collection('Users/' + user.uid + '/' + shoppingList).get().then(userList => {
-                        let item = userList.docs;
-                        for (i = 0; i < item.length; i++) {
+                        listHTML = 
+                        '<tr id="itemContainer' + i + '">'+
+                            '<td data-th="Product">' +
+                                //The item image
+                                '<image id="image' + i + '" width="100px "height="100" >' +
+                                    //Item Name
+                                '<h4 class="nomargin">' +
+                                    '<span id="itemName' + i + '">' + item[i].get("name") + '</span>'+
+                                '</h4> ' +
+                            '</td>' +
+                            //Edit qty buttons
+                            '<td class="editButtons">' +
+                                '<button class="btn btn-primary" id="increase' + i + '">' +
+                                    '<i class="fa fa-plus"></i>'+
+                                '</button>' +
+                                //item qty
+                                '<h1 id="qtyNum' + i + '"class="qtyNum">'+
+                                    item[i].get("qty")+
+                                '</h1>' +
+                                '<button class="btn btn-primary" id="decrease' + i + '" >' +
+                                    '<i class="fas fa-minus"></i>' +
+                                '</button>' +
+                            '</td>' +
+                            // unavailable of delete button
+                            '<td class="statusButtons">' +
+                                '<button class="btn btn-success" id="remove' + i + '">' +
+                                    '<i class="fa fa-check"></i>'+
+                                '</button>' +
+                                '<button class="btn btn-danger" id="add' + i + '" >' +
+                                    '<i class="fas fa-times"></i>' +
+                                '</button>' +
+                            '</td>' +
+                        '</tr>';
 
-                            listHTML = 
-                            '<tr id="itemContainer' + i + '">'+
-                                '<td data-th="Product">' +
-                                    //The item image
-                                    '<image id="image' + i + '" width="100px "height="100" >' +
-                                        //Item Name
-                                    '<h4 class="nomargin">' +
-                                        '<span id="itemName' + i + '">' + item[i].get("name") + '</span>'+
-                                    '</h4> ' +
-                                '</td>' +
-                                '<td data-th="">' +
-                                '</td>' +
-                                '<td class="actions" id="action">' +
-                                    // available or qty button
-                                    '<button class="btn btn-success" id="remove' + i + '">' +
-                                        '<i class="fa fa-check"></i></button>' +
-                                '</td>' +
-                                    // unavailable of delete button
-                                '<td class="actions" id="action">' +
-                                    '<button class="btn btn-danger" id="add' + i + '" >' +
-                                    '   <i class="fas fa-times"></i>' +
-                                    '</button>' +
-                                '</td>' +
-                            '</tr>';
+                        let itemName = item[i].get("name");
+                        let containerName = "#itemContainer" + i;
+                        let nameId = '#itemName' + i;
+                        let increaseId = '#increase' + i;
+                        let decreaseId = '#decrease' + i;
+                        let qtyNum = '#qtyNum' + i;
 
-                            let itemName = item[i].get("name");
-                            let containerName = "#itemContainer" + i;
-                            let nameId = '#itemName' + i;
+                        let str = itemName.toString();
 
-                            let str = itemName.toString();
+                        $('#itemList').append(listHTML);
+                        listHTML = "";
 
-                            $('#itemList').append(listHTML);
-                            listHTML = "";
-                            $(document).on('click', "#remove" + i, () => {
-                                $(containerName).css("background-color", "green");
-                                $(nameId).removeClass('unavailable');
-                                console.log("removing " + itemName + " from " + currentStore + " unavailable list");
-                                removeItemFromUnavailable(itemName);
-                            });
+                        //
+                        $(document).on('click', "#remove" + i, () => {
+                            $(containerName).addClass("contAvail");
+                            $(containerName).removeClass("contUnavail");
+                            $(nameId).removeClass('unavailable');
+                            console.log("removing " + itemName + " from " + currentStore + " unavailable list");
+                            removeItemFromUnavailable(itemName);
+                        });
+                        //
 
+                        $(document).on('click', increaseId, () => {
+                            $(qtyNum).text(parseInt($(qtyNum).text(), 10) + 1);
+                        });
 
-                            $(document).on('click', "#add" + i, () => {
-                                $(containerName).css("background-color", "red");
-                                $(nameId).addClass('unavailable');
-                                console.log("adding " + itemName + "  to " + currentStore + " unavailable list");
-                                addItemToUnavailable(itemName);
-                            });
+                        $(document).on('click', decreaseId, () => {
+                            if ($(qtyNum).text() !== '0') {
+                                $(qtyNum).text(parseInt($(qtyNum).text(), 10) - 1);
+                            } else {
 
-                            // We could host these images in firebase, but we have a read limit so we hardcode them here
-                            if ((str === "Apple")) {
-                                $('#image' + [i]).attr('src', 'CSS/itemimage/apple.jpg');
                             }
-                            if ((str === "Beans")) {
+                        });
 
-                                $('#image' + [i]).attr('src', 'CSS/itemimage/Beans.jpg');
-                            }
-                            if ((str === "Canned Vegetables")) {
-
-                                $('#image' + [i]).attr('src', 'CSS/itemimage/Canned Vegetables.jpg');
-                            }
-                            if ((str === "Detergent")) {
-
-                                $('#image' + [i]).attr('src', 'CSS/itemimage/Detergent.jpg');
-                            }
-                            if ((str === "Disinfectant Wipes")) {
-
-                                $('#image' + [i]).attr('src', 'CSS/itemimage/Disinfectant Wipes.jpeg');
-                            }
-                            if ((str === "Eggs")) {
-
-                                $('#image' + [i]).attr('src', 'CSS/itemimage/Eggs.jpg');
-                            }
-                            if ((str === "Face Masks")) {
-
-                                $('#image' + [i]).attr('src', 'CSS/itemimage/Face Masks.jpeg');
-                            }
-                            if ((str === "Ground Beef")) {
-
-                                $('#image' + [i]).attr('src', 'CSS/itemimage/Ground Beef.jpg');
-                            }
-                            if ((str === "Hand Sanitizer")) {
-
-                                $('#image' + [i]).attr('src', 'CSS/itemimage/Hand Sanitizer.jpg');
-                            }
-                            if ((str === "Orange")) {
-
-                                $('#image' + [i]).attr('src', 'CSS/itemimage/Orange.jpg');
-                            }
-                            if ((str === "Pasta")) {
-
-                                $('#image' + [i]).attr('src', 'CSS/itemimage/Pasta.jpg');
-                            }
-                            if ((str === "Potato")) {
-
-                                $('#image' + [i]).attr('src', 'CSS/itemimage/Potato.jpg');
-                            }
-                            if ((str === "Rice")) {
-
-                                $('#image' + [i]).attr('src', 'CSS/itemimage/Rice.jpg');
-                            }
-                            if ((str === "Soap")) {
-
-                                $('#image' + [i]).attr('src', 'CSS/itemimage/Soap.jpg');
-                            }
-                            if ((str === "Toilet Paper")) {
-
-                                $('#image' + [i]).attr('src', 'CSS/itemimage/Toilet Paper.jpg');
-                            }
-                            if ((str === "Tylenol")) {
-                                $('#image' + [i]).attr('src', 'CSS/itemimage/Tylenol.jpg');
-                            }
-                            if ((str === "Uno Card Game")) {
-
-                                $('#image' + [i]).attr('src', 'CSS/itemimage/Uno Card Game.jpg');
-                            }
-                            if ((str === "Vitamin")) {
-
-                                $('#image' + [i]).attr('src', 'CSS/itemimage/Vitamin.jpg');
-                            }
-                        }
-                        console.log(array);
+                        $(document).on('click', "#add" + i, () => {
+                            $(containerName).removeClass("contAvail");
+                            $(containerName).addClass("contUnavail");
+                            $(nameId).addClass('unavailable');
+                            console.log("adding " + itemName + "  to " + currentStore + " unavailable list");
+                            addItemToUnavailable(itemName);
+                        });
+                        appendImage(str, i);
+                    }
 
 
-                    });
                 });
             });
         });
+
     }
     displayList();
-    $(document).on('click', '#editMode', () => {
-        shoppingMode = !shoppingMode;
-        if (!shoppingMode) {
-            $('.editing').css('display', 'default');
-        } else {
-            $('.editing').css('display', 'none');
-        }
-    })
 
 });
 
-function edit() {
-    
+
+// Function that appends images to list items
+// We could host these images in firebase, but we have a read limit so we hardcode them here
+function appendImage(str, i) {
+    if ((str === "Apple")) {
+        $('#image' + [i]).attr('src', 'CSS/itemimage/apple.jpg');
+    }
+    if ((str === "Beans")) {
+        $('#image' + [i]).attr('src', 'CSS/itemimage/Beans.jpg');
+    }
+    if ((str === "Canned Vegetables")) {
+        $('#image' + [i]).attr('src', 'CSS/itemimage/Canned Vegetables.jpg');
+    }
+    if ((str === "Detergent")) {
+        $('#image' + [i]).attr('src', 'CSS/itemimage/Detergent.jpg');
+    }
+    if ((str === "Disinfectant Wipes")) {
+        $('#image' + [i]).attr('src', 'CSS/itemimage/Disinfectant Wipes.jpeg');
+    }
+    if ((str === "Eggs")) {
+        $('#image' + [i]).attr('src', 'CSS/itemimage/Eggs.jpg');
+    }
+    if ((str === "Face Masks")) {
+        $('#image' + [i]).attr('src', 'CSS/itemimage/Face Masks.jpeg');
+    }
+    if ((str === "Ground Beef")) {
+        $('#image' + [i]).attr('src', 'CSS/itemimage/Ground Beef.jpg');
+    }
+    if ((str === "Hand Sanitizer")) {
+        $('#image' + [i]).attr('src', 'CSS/itemimage/Hand Sanitizer.jpg');
+    }
+    if ((str === "Orange")) {
+        $('#image' + [i]).attr('src', 'CSS/itemimage/Orange.jpg');
+    }
+    if ((str === "Pasta")) {
+        $('#image' + [i]).attr('src', 'CSS/itemimage/Pasta.jpg');
+    }
+    if ((str === "Potato")) {
+        $('#image' + [i]).attr('src', 'CSS/itemimage/Potato.jpg');
+    }
+    if ((str === "Rice")) {
+        $('#image' + [i]).attr('src', 'CSS/itemimage/Rice.jpg');
+    }
+    if ((str === "Soap")) {
+        $('#image' + [i]).attr('src', 'CSS/itemimage/Soap.jpg');
+    }
+    if ((str === "Toilet Paper")) {
+        $('#image' + [i]).attr('src', 'CSS/itemimage/Toilet Paper.jpg');
+    }
+    if ((str === "Tylenol")) {
+        $('#image' + [i]).attr('src', 'CSS/itemimage/Tylenol.jpg');
+    }
+    if ((str === "Uno Card Game")) {
+        $('#image' + [i]).attr('src', 'CSS/itemimage/Uno Card Game.jpg');
+    }
+    if ((str === "Vitamin")) {
+        $('#image' + [i]).attr('src', 'CSS/itemimage/Vitamin.jpg');
+    }
+
 
 }
 
